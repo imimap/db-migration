e09b1bbdb18f2ad86df0e01fc5ef690f2c52cd73
david <ds@jzz.es>
2023-06-19 12:43:15 +0200

    docker befehle für imi-map

--- import.sh
+++ import.sh
@@ -2,3 +2,6 @@ docker compose up -d
 docker cp  ./db-dump.gz  db-migration-db-mongo-1:
 docker exec db-migration-db-mongo-1 mongorestore  --gzip --archive=db-dump.gz
 docker compose down
+
+//docker cp db-dump_withadmin.gz imimap21-db-1:/
+//docker exec imimap21-db-1 mongorestore  --gzip --drop --archive=db-dump_withadmin.gz
